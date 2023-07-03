6e8e49c2d6ccf1b317c3d50bd7a56eb1b3dfad98
Sagar Gurung <sgrgug@gmail.com>
2023-07-03 14:36:50 +0545

    first commit

---
+++ resources/views/admin/includes/header.blade.php
@@ -0,0 +1,47 @@
+<header class="topbar">
+    <nav class="navbar top-navbar navbar-expand-md navbar-light">
+        <!-- ============================================================== -->
+        <!-- Logo -->
+        <!-- ============================================================== -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.html">
+                <!-- Logo icon -->
+                <b>
+                    <img src="{{ asset('site/assets/image/logo.png') }}" alt="homepage" class="dark-logo" style="width: 250px;" />
+                    {{-- OPH --}}
+                </b>
+                <!--End Logo icon -->
+              </div>
+        <!-- ============================================================== -->
+        <!-- End Logo -->
+        <!-- ============================================================== -->
+        <div class="navbar-collapse">
+            <!-- ============================================================== -->
+            <!-- toggle and nav items -->
+            <!-- ============================================================== -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up waves-effect waves-dark" href="javascript:void(0)"><i class="fa fa-bars"></i></a> </li>
+                <!-- ============================================================== -->
+                <!-- Search -->
+                <!-- ============================================================== -->
+                <li class="nav-item hidden-xs-down search-box"> <a class="nav-link hidden-sm-down waves-effect waves-dark" href="javascript:void(0)"><i class="fa fa-search"></i></a>
+                    <form class="app-search">
+                        <input type="text" class="form-control" placeholder="Search & enter"> <a class="srh-btn"><i class="fa fa-times"></i></a></form>
+                </li>
+            </ul>
+            <!-- ============================================================== -->
+            <!-- User profile and search -->
+            <!-- ============================================================== -->
+            <ul class="navbar-nav my-lg-0">
+                <!-- ============================================================== -->
+                <!-- Profile -->
+                <!-- ============================================================== -->
+                <li class="nav-item dropdown u-pro">
+                    <a class="nav-link dropdown-toggle waves-effect waves-dark profile-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="hidden-md-down">{{ Auth::user()->name }}</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('site/assets/image/favicon.ico') }}" type="image/x-icon">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
+    <title>नेपाल सरकार,प्रदेश प्रमुखकाे कार्यालय
+        गण्डकी प्रदेश, पाेखरा</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('site/admin/assets/node_modules/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('site/admin/assets/node_modules/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <!-- This page CSS -->
+    <!-- chartist CSS -->
+    <link href="{{ asset('site/admin/assets/node_modules/morrisjs/morris.css') }}" rel="stylesheet">
+    <!--c3 CSS -->
+    <link href="{{ asset('site/admin/assets/node_modules/c3-master/c3.min.css') }}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ asset('site/admin/css/style.css') }}" rel="stylesheet">
+    <!-- Dashboard 1 Page CSS -->
+    <link href="{{ asset('site/admin/css/pages/dashboard1.css') }}" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="{{ asset('site/admin/css/colors/default.css') }}" id="theme" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <main>
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{{ asset('site/admin/assets/node_modules/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap popper Core JavaScript -->
+    <script src="{{ asset('site/admin/assets/node_modules/bootstrap/js/popper.min.js') }}"></script>
+    <script src="{{ asset('site/admin/assets/node_modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{ asset('site/admin/js/perfect-scrollbar.jquery.min.js') }}"></script>
+    <!--Wave Effects -->
+    <script src="{{ asset('site/admin/js/waves.js') }}"></script>
+    <!--Menu sidebar -->
+    <script src="{{ asset('site/admin/js/sidebarmenu.js') }}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{ asset('site/admin/js/custom.min.js') }}"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!--morris JavaScript -->
+    <script src="{{ asset('site/admin/assets/node_modules/raphael/raphael-min.js') }}"></script>
+    <script src="{{ asset('site/admin/assets/node_modules/morrisjs/morris.min.js') }}"></script>
+    <!--c3 JavaScript -->
+    <script src="{{ asset('site/admin/assets/node_modules/d3/d3.min.js') }}"></script>
+    <script src="{{ asset('site/admin/assets/node_modules/c3-master/c3.min.js') }}"></script>
+    <!-- Chart JS -->
+    <script src="{{ asset('site/admin/js/dashboard1.js') }}"></script>
+</body>
+</html>
